d4e853e79121e3fd932bbb43ce4cb8eb0b4fe309
Christoph Pahmeyer <christoph.pahmeyer@ilr.uni-bonn.de>
2025-12-02 15:13:06 +0100

    update types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,11 +1,15 @@
 export interface HarmonieQuery {
   state: string;
   xml?: string;
+  gml?: string;
   shp?: ArrayBuffer;
   dbf?: ArrayBuffer;
   prj?: string;
+  dat?: string;
   /** Encoding of the shapefile */
   encoding?: string;
   /** Sometimes a projection is passed as a string??? */
   projection?: string;
+  /** Mapping for generic shapefile handler */
+  mapping?: Record<string, string>;
 }
